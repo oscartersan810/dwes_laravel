57bc763b7f40e620fe6ec4202929fcda8cf37558
oscartersan810 <oscartersan810@gmail.com>
2025-03-04 09:52:35 +0100

    Subidos los proyectos Laravel

---
+++ proyectoLaravel_GestorMV/database/migrations/2025_03_04_081120_create_usuarios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('apellidos', 100);
+            $table->string('apodo', 100);
+            $table->integer('edad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
---
+++ proyectoLaravel_GestorMV/database/migrations/2025_03_04_081122_create_maquinas_virtuales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maquinas_virtuales', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('so', 50);
+            $table->string('ram', 50);
+            $table->string('cpu', 50);
+            $table->string('discoduro', 50);
+            $table->string('tipoRed', 50);
+            $table->string('ip', 50);
+            $table->foreignId('id_usuario')->constrained('usuarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maquinas_virtuales');
+    }
+};
